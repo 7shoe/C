5173c2067e3173a60c532e4203ad012c2367ca1c
7shoe <carlo.siebenschuh@gmail.com>
2022-08-09 22:57:13 -0500

    Chapter 1, VIII

---
+++ 01_Ritchie_Kernighan/Chapter 2/2.1 Range of datatypes.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define MAXLEN 100000000
+
+int intRange(){
+
+    long prev, lower, upper;
+    lower = 0;
+    upper = 0;
+    prev = 0; 
+    for(int k = 1; k < MAXLEN; ++k){
+        printf("%d\n", (int)k - (int)prev);
+        if((int)k - (int)prev != 1){
+            upper = prev;
+            printf("break");
+            break;
+        }
+        prev++;
+    }
+
+    prev = 0;
+    for(int k = -1; k > -1 * MAXLEN; --k){
+        printf("%d\n", (int)k - (int)prev);
+        if((int)k - (int)prev != -1){
+            lower = prev;
+            printf("break");
+            break;
+        }
+        prev = k;
+    }
+
+    printf("int Range: {%ld, ..., %ld}", lower, upper);
+
+    return 0;
+}
+
+
+int main(){
+
+
+    // int Range
+    intRange(); 
+
+    return 0;
+}
