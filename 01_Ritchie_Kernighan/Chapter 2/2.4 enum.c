411f47dd9cc376d986a0419b6d2710c0bdb07a61
7shoe <carlo.siebenschuh@gmail.com>
2022-08-11 22:07:00 -0500

    Chapter 2, III

---
+++ 01_Ritchie_Kernighan/Chapter 2/2.4 enum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+    enum months {JAN=1, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC};
+
+    enum months currentMonth;
+    currentMonth = AUG;
+
+    printf('Current Month is the %d-th month of the year!: ', currentMonth);
+
+
+    return 0;
+}
