fb5b036701ea2101231fa91ea1ebd350b38999e1
7shoe <carlo.siebenschuh@gmail.com>
2022-08-12 22:51:31 -0500

    Chapter 2, XV

---
+++ 01_Ritchie_Kernighan/Chapter 2/2.7 Type conversion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* convert c to lower-case */
+int lower(int c){
+
+    if(c >= 'A' && c <= 'Z'){
+        return c + 'a' - 'A';
+    }else{
+        return c;
+    }
+}
+
+int main(){
+
+
+    printf("%c", lower('D'));
+
+    return 0;
+}
