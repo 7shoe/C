a674eae8aa4215fddb1ff4f47138e644d95e0b56
7shoe <carlo.siebenschuh@gmail.com>
2022-08-13 23:31:48 -0500

    Chapter 2, XXIX

--- 01_Ritchie_Kernighan/Chapter 2/2.11 Conversion.c
+++ 01_Ritchie_Kernighan/Chapter 2/2.11 Conversion.c
@@ -14,5 +14,22 @@ int main(){
 
     printf("sqrt(144.0f)=%.1f\n", sqrt(144.0f));
 
+    // equally works: convert output to (int) if print with %d
+    printf("(int)sqrt(144)=%d\n", (int)sqrt(144)); // returns (non-sense) for %d:  sqrt(144)=-1074792800
+
+    printf("sqrt((double)144)=%d\n", (int)sqrt((double)144));
+
+    printf("sqrt((float)144)=%d\n", (int)sqrt((float)144));
+
+    printf("sqrt(144.0)=%d\n", (int)sqrt(144.0));
+
+    printf("sqrt(144.2f)=%d\n", (int)sqrt(2.25f));  // conversion even when non-integer output (int) just rounds
+
+    // Round through (int)
+    double x1 = 12.5678923675;
+
+    printf("(int)x = %d", (int)x1);
+    
+
     return 0;
 }
