51019880813767b9bf87ae8ec3491710959a5798
7shoe <carlo.siebenschuh@gmail.com>
2022-08-15 23:45:20 -0500

    XXXXIII

--- 01_Ritchie_Kernighan/Chapter 2/2.15 Bit TraFos.c
+++ 01_Ritchie_Kernighan/Chapter 2/2.15 Bit TraFos.c
@@ -5,18 +5,18 @@
 void printBits(int x){
     int log2x = (int)log2((double)x)+1;
 
-    printf("%d = (", x);
+    printf("%d = {", x);
     for(int k=log2x-1; k > 0; --k){
         printf("%d,", ((x & (1 << k)) >> k));
     }
-    printf("%d)\n", ((x & (1 << 0)) >> 0));
+    printf("%d}\n", ((x & (1 << 0)) >> 0));
 }
 
 /* Turn bit representation to integer */
 void printIntNB(int arr[10]){
     int y = 0;
     for(int k=0; k < 10; ++k){
-        y += arr[k] * powl(2, k);
+        y += arr[10-k-1] * powl(2, k);
     }
     printf("y = %d\n", y);
 }
@@ -27,7 +27,7 @@ int main(){
     printBits(666);
 
     // Check: bits -> int 
-    int a[10] = {0,0,0,1,1,1,0,1,0,0};
+    int a[10] = {1,0,1,0,0,1,1,0,1,0};
     printIntNB(a);
 
 
