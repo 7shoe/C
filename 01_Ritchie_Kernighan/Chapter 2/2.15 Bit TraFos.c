3ab7648598176c2dd500ef8735f30c8556756254
7shoe <carlo.siebenschuh@gmail.com>
2022-08-15 23:34:39 -0500

    XXXXI

--- 01_Ritchie_Kernighan/Chapter 2/2.15 Bit TraFos.c
+++ 01_Ritchie_Kernighan/Chapter 2/2.15 Bit TraFos.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 
+/* Print the bit representation of an arbitrary integer x */
 void printBits(int x){
     int log2x = (int)log2((double)x)+1;
 
     printf("%d = (", x);
-    for(int k=1; k < log2x; ++k){
-        printf("%d,", ((x & (1 << (k - 1))) >> (k - 1)));
+    for(int k=log2x-1; k > 0; --k){
+        printf("%d,", ((x & (1 << k)) >> k));
     }
-    printf("%d)\n", ((x & (1 << log2x)) >> log2x));
-    
+    printf("%d)\n", ((x & (1 << 0)) >> 0));
+}
 
+/* Turn bit representation to integer */
+void printIntNotBitwise(int arr[10]){
+    int y = 0;
+    for(int k=0; k < 10; ++k){
+        y += powl(2, k);
+    }
+    printf("y = %d", y);
 }
 
 int main(){
 
-    printBits(15);
+    printBits(666);
 
 
 
