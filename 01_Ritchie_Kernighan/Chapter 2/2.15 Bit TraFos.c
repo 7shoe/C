d7db4a2ecf0a40ad1ecdd53a65503d32c885574b
7shoe <carlo.siebenschuh@gmail.com>
2022-08-17 17:07:00 -0500

    LIV

--- 01_Ritchie_Kernighan/Chapter 2/2.15 Bit TraFos.c
+++ 01_Ritchie_Kernighan/Chapter 2/2.15 Bit TraFos.c
@@ -31,43 +31,18 @@ void printInt(int arr[10]){
 }
 
 void bitAdder(int x, int y){
-    int s, z, d, log2x, log2y;
-    if(y > x){
-        s = y;
-        y = x;
-        x = s;
-    }
+    int a, b, c;
+    a = x;
+    b = y;
 
-    // log
-    log2x = (int)log2((double)x) + 1;
-    log2y = (int)log2((double)y) + 1;
-    z = 0;
-
-    printf("log2x=%d\n", log2x);
-    printf("log2y=%d\n", log2y);
-    printf("d=%d\n", d);
-    printBits(x);
-    printBits(y);
-
-    s = 0;
-    for(int k=log2x; k >= 0; --k){
-        printf("%d, ", (x & (1 << k))); 
-    }
-    printf("\n");
-    for(int k=log2x; k >= 0; --k){
-        if(k <= log2y){
-            printf("%d, ", (y & (1 << k)));
-        }else{
-            printf("%d, ", 0);
-        } 
-    }
-    printf("\n");
-    for(int k=log2x; k >= 0; --k){
-         printf("%d, ", (y & (1 << (k+d-1))));
+    while(y!=0){
+        c = x & y;
+        x = x ^ y;
+        y=c << 1;
     }
 
     // output sum
-    //printf("%d + %d = %d", x, y, s);
+    printf("%d + %d = %d", a, b, x);
 }
 
 // 1010111 # 87
