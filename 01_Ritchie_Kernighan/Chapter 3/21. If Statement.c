c7de3e459b6cec1086663a3265567c42061ebfb4
7shoe <carlo.siebenschuh@gmail.com>
2022-08-17 23:32:32 -0500

    Chapter 3, I

--- 01_Ritchie_Kernighan/Chapter 3/21. If Statement.c
+++ 01_Ritchie_Kernighan/Chapter 3/21. If Statement.c
@@ -6,15 +6,32 @@ int main(){
 
 
     int i, j, k, l;
-    i = 10;
+    i = 11;
     j = 5;
 
-    // works! Not {} needed for a one-line statement after if(expression)
+    // WORKS! Not {} needed for a one-line statement after if(expression)
     if(i < j)
         printf("i < j is true!\n");
-        printf("This line will always be printed!");
-    //else
-    //    printf("It holds: i >= j.\n");
+
+    // 2nd statement (and beyond) ALWAYS executed after an if-clause when no curly brackets ({}) are used!
+    if(i < j)
+        printf("i < j is true!\n");
+        printf("This line will always be printed!\n");
+
+    // WORKS: NO CURLY BRAKETS needed if if() and else() are both one-liners
+    if(i < j)
+        printf("i < j!\n");
+    else
+        printf("not true.\n");
+
+    // ONLY WORKS ... when the 2nd statement in the if() is commented out. Otherwise  "error: expected expression"
+    if(i < j)
+        printf("i < j!\n");
+        //printf("Always printed... always!\n");
+    else
+        printf("not true.\n");
+        printf("Holds true for the else() as well: Always printed... always!\n");
+    
 
 
     return 0;
