53c2b52da5059f4c60baccf3708de8ff2cf38afc
7shoe <carlo.siebenschuh@gmail.com>
2022-08-19 23:56:57 -0500

    Chapter 4, XIII

---
+++ 01_Ritchie_Kernighan/Chapter 4/4.3 External Variable.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// external variable since defined outside the function
+int globalCount = 0;
+
+int dancer(){
+    globalCount++;
+
+    return 1;
+}
+
+dummy(){globalCount++;}
+
+int dummy2(){
+    globalCount++;
+
+    return 0;
+}
+
+int main(){
+
+
+    printf("Global Variable:\nglobalCount: %d\n", globalCount);
+
+    dancer();
+    dancer();
+    dummy();
+    dummy2();
+    dancer();
+    dummy2();
+
+    printf("globalCount: %d\n", globalCount);
+
+
+    return 0;
+}
