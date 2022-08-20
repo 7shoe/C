862844def025f90c7591344fc4e3c1fcee343ccd
7shoe <carlo.siebenschuh@gmail.com>
2022-08-19 23:19:56 -0500

    Chapter 4, X

--- 01_Ritchie_Kernighan/Chapter 4/4.2 Scientific Notation.c
+++ 01_Ritchie_Kernighan/Chapter 4/4.2 Scientific Notation.c
@@ -14,66 +14,85 @@ int strindex(char s[], char t, int n){
 /* scientific to float*/
 float getScience(char s[]){
 
-    int len = strlen(s);
-    double num = 0.0;
-    double ten = 1.0;
-    int sPoint, sExp, sexp, sMinus, EXP;
+    double num, ten;
+    int len, sPoint, sExp, sexp, sMinus, EXP, expTen;
+
+    len = strlen(s);
+
+    // printf("len: %d\n", len);
     
     sPoint = strindex(s, '.', len);
     sExp = strindex(s, 'E', len);
     sexp = strindex(s, 'e', len);
     sExp = (sExp > sexp) ? sExp : sexp;
-    ten = 1;
+    
+
+    //printf("sPoint: %d\n", sPoint);
+    //printf("sExp: %d\n", sExp);
+    //printf("sexp: %d\n", sexp);
+    //printf("len: %d\n", len);
 
     // mantissa (pre-decimal)
+    num = 0.0;
+    ten = 1.0;
     if(sPoint != -1){
         for(int i=sPoint-1; i >=0; --i){
-            num += ten * (int)s[i];
-            ten *= 10.0;
+            // very convenient of proper conversion of char to int. Note: (int)'9' = 57 while (int)'0' = 58. In turn, '9' - '0' = 9
+            num += (s[i] - '0') * ten;    // char c = '5'; Then, (c - '0') will convert it to int!
+            ten = ten * 10;
         }
     }else{
         for(int i=sExp-1; i >=0; --i){
-            num += ten * (int)s[i];
-            ten *= 10.0;
+            num += (s[i] - '0') * ten; 
+            ten *= 10;
         }
     }
+    //printf("num = %f\n", num);
+    //printf("\n\n\n");
+
     // mantissa (post-decimal)
     if((sPoint!=-1) && (sExp > sPoint)){
         ten = 0.1;
-        for(int i=sExp-1; i > sPoint; --i){
-            num += ten * (int)s[i];
-            ten /= 10.0;
+        for(int i=sPoint+1; i < sExp; ++i){
+            num += ten * (s[i] - '0');
+            ten *= 0.1;
         }
     }
 
+    printf("num = %f\n", num);
+    //printf("%c = %d", '0', (int)'0');
+
     // exponent
     if((sMinus!=-1) && (sExp!=-1))
-        sExp += 1;
-    ten = 1.0;
-    for(int i=sExp + 1; i < len; ++i){
-        EXP += ten * (double)s[i];
-        ten *= 10.0;
+        sExp += 2;
+    expTen = 1;
+    for(int i=sExp ; i < len; ++i){
+        EXP += expTen * (s[i] - '0');
+        expTen *= 10;
     }
+    printf("EXP = %d\n", EXP);
     if(sMinus==-1)
         for(int i=0; i < EXP; ++i)
             num *= 10.0;
     else
         for(int i=0; i < EXP; ++i)
-            num /= 10.0;
+            num *= 0.1;
+
+    printf("num = %f\n", num);
 
     return num;
 }
 
 int main(){
 
-    char str1[] = "1.23e-6";
+    char str1[] = "1234.23e-6";
     char str2[] = "1.2E3";
 
 
 
     printf("s1: %f", getScience(str1));
 
-    printf("\nstr1 = %s", str1);
+    printf("\n\nPure String Output:\nstr1 = %s", str1);
 
 
 
