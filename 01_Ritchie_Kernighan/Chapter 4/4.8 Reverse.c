88377acac262996e3da44d646d5490e65d79cf77
7shoe <carlo.siebenschuh@gmail.com>
2022-08-20 22:13:35 -0500

    Chapter 4, XXI

--- 01_Ritchie_Kernighan/Chapter 4/4.8 Reverse.c
+++ 01_Ritchie_Kernighan/Chapter 4/4.8 Reverse.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
+
+char s[] = "Rentner";
 
 /*  */
-void reverse(char s[]){
+void reverse(){
     char p;
     int i, n;
 
     i = 0;
     n = strlen(s);
+
+    // printf("n = %d", n);
 
     while(n-i > 2){
         p = s[n];
@@ -22,14 +27,26 @@ void reverse(char s[]){
 
 int main(){
 
-    char s1[] = "Hello";
+    static char s1[] = "Hello";
     char s2[] = "Rentner";
+    char s3[] = "Reittier";
+    char s4[] = "livid";
 
-    printf("reverse(%s) = ", s1);
-    reverse(s1);
-    printf("")
+    printf("reverse(%s) = ", s);
+    reverse();
+    printf("%s\n", s);
 
+    printf("reverse(%s) = ", s2);
+    reverse();
+    printf("%s\n", s2);
 
+    printf("reverse(%s) = ", s3);
+    reverse();
+    printf("%s\n", s3);
+
+    printf("reverse(%s) = ", s4);
+    reverse();
+    printf("%s\n", s4);
 
     return 0;
 }
