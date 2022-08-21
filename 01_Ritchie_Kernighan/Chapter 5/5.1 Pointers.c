6561636053574cee6983b3150db7a98edcf35d21
7shoe <carlo.siebenschuh@gmail.com>
2022-08-20 22:39:04 -0500

    Chapter 5, III

--- 01_Ritchie_Kernighan/Chapter 5/5.1 Pointers.c
+++ 01_Ritchie_Kernighan/Chapter 5/5.1 Pointers.c
@@ -28,7 +28,24 @@ int main(){
     printf("ip : %p\n", ip);
     printf("jp : %p\n", jp);
 
-    
+    // Experiment 1: Assign values to value via *pointer
+    // *ip += 1
+    printf("*ip : %d\n", *ip);
+    printf("*ip += 1;\n");
+    *ip += 1;
+    printf("*ip : %d\n", *ip);
+
+    // Experiment 2: Assign value to variable to which pointer points
+    // x+=1;
+    x+=1;
+    printf("x+=1;\n");
+    printf("*ip : %d\n", *ip);
+
+    // Experiment 3: Assign 
+
+
+
+
 
 
 
