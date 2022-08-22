2e17ad6c44c09d396be8a950ba5d20a876bf00f0
7shoe <carlo.siebenschuh@gmail.com>
2022-08-22 13:09:30 -0500

    Chapter 5, XVII

--- 01_Ritchie_Kernighan/Chapter 5/5.12 Multidimensional Array.c
+++ 01_Ritchie_Kernighan/Chapter 5/5.12 Multidimensional Array.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+static char daytab[2][13] = {
+        {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},    // leap year
+        {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},    // non-leap year
+    };
+
 /* isLeap : return if a given year is a leap year */
 int isLeap(int year){
     if((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0)))
@@ -8,19 +13,24 @@ int isLeap(int year){
 }
 /* day_of_year : set day from month & day */
 int day_of_year(int year, int month, int day){
-    int i, leap;
+    int i, leap, sum;
 
-    leap = (isLeap)
+    leap = isLeap(year);
+    sum = 0;
+    i = 0;
+
+    while(i < month)
+        sum += daytab[leap][i++];
+
+    return sum + day;
+
 }
 
 
 int main(){
 
-    static char daytab[2][13] = {
-        {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},    // leap year
-        {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},    // non-leap year
-    };
 
+    printf("Day of the year: %d\n", day_of_year(2022, 8, 22));
 
 
 } 
