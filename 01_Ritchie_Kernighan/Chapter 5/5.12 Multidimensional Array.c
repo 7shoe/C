48d5beef1181cccbeaa8301b1736bd4dce641ed9
7shoe <carlo.siebenschuh@gmail.com>
2022-08-22 12:31:12 -0500

    Chapter 5, XVI

---
+++ 01_Ritchie_Kernighan/Chapter 5/5.12 Multidimensional Array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* isLeap : return if a given year is a leap year */
+int isLeap(int year){
+    if((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0)))
+        return 1;
+    return 0;
+}
+/* day_of_year : set day from month & day */
+int day_of_year(int year, int month, int day){
+    int i, leap;
+
+    leap = (isLeap)
+}
+
+
+int main(){
+
+    static char daytab[2][13] = {
+        {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},    // leap year
+        {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},    // non-leap year
+    };
+
+
+
+} 
