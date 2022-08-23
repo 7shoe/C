f124b4f956210e34996a85e44be0b3577062d436
7shoe <carlo.siebenschuh@gmail.com>
2022-08-22 23:50:10 -0500

    Chapter 5, XXIII

--- 01_Ritchie_Kernighan/Chapter 5/5.17 Functions and Pointers.c
+++ 01_Ritchie_Kernighan/Chapter 5/5.17 Functions and Pointers.c
@@ -14,14 +14,16 @@ int *f(int n){
 }
 
 
-
 int main(){
 
     printf("(%d)**2 = %ld\n", 12, squareInt(12));
     printf("(%d)**2 = %ld\n\n", 132, squareInt(132));
 
     printf("(%d)**2 = %p\n", 132, f(132));   // prints adress of the argument provided
-    printf("(%d)**2 = %d\n", 132, *f(132));  // prints content of cell (the output pointer is pointing at)
+    printf("(%d)**2 = %d\n\n", 132, *f(132));  // prints content of cell (the output pointer is pointing at)
+
+    int (*fun_ptr)(int) = &squareInt;
+     printf("(%d)**2 = %d\n", 132, (*fun_ptr)(132));
 
 
 
