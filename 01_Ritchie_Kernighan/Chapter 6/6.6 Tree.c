e131c3d74114feaad15fced9f44536eadf974ea1
7shoe <carlo.siebenschuh@gmail.com>
2022-08-24 14:32:07 -0500

    Chapter 6, VII

---
+++ 01_Ritchie_Kernighan/Chapter 6/6.6 Tree.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+struct treeNode {
+    int number;
+    int count;
+    struct treeNode *left;
+    struct teeeNode *right;
+};
+
+/* parseInt: parse integer into the binary tree s */
+void parseInt(struct treeNode *tree, int x){
+    // parse through tree. each node: check >=. If 'yes' go to right ptr. Else left ptr.
+    struct treeNode *lastNode;
+
+    //if(tree==NULL){
+    //    struct treeNode currNode = {.number = x, .count = 1};
+    while((tree!=NULL) && (tree->number) != x){
+        lastNode = tree->number;
+        if(x > tree->number)
+            tree = tree->left;
+        else
+            tree = tree->right;
+    }
+    if(tree==NULL){
+         struct treeNode currNode = {.number = x, .count = 1};   // no match: create new node
+         if(lastNode->number > x)
+            lastNode->left = &currNode;
+        else
+            lastNode->right = &currNode;
+    }
+    if((tree->number) == x)
+        ++tree->count;         // match! increment count of that node's number
+}
+
+
+
+int main(){
+
+    // Task: Given an array arr[], count number of occurences on an integer. Entries are unknown a-priori.
+    int arr[] = {10, 7, 6}; //tacitely assumes vallues > 0
+
+    // empty tree (list of node pointers)
+    //static int N = sizeof(arr) /sizeof(arr[0]); 
+    struct treeNode *tree = NULL;
+
+    
+    printf("tree ptr: %p\n", tree);
+    printf("tree ptr is NULL? %d\n", tree==NULL);
+    printf("n1.left  is NULL? %d\n", n1.left==NULL);
+    printf("n1.right is NULL? %d\n", n1.right==NULL);
+
+    pritnf();
+    
+
+
+
+    return 0;
+}
