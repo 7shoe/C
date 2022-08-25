472edcc85f77339d0e23be939f3fcd9cf58b7708
7shoe <carlo.siebenschuh@gmail.com>
2022-08-24 23:51:30 -0500

    Chapter 6, XIII

--- 01_Ritchie_Kernighan/Chapter 6/6.7 table Lookup.c
+++ 01_Ritchie_Kernighan/Chapter 6/6.7 table Lookup.c
@@ -13,6 +13,24 @@ struct nlist {
 static struct nlist *hastab[HASHSIZE]; // pointer table
 
 /* hash */
+unsigned hash(char *s){
+    unsigned hashval;
+
+    for(hashval = 0; *s != '\0'; s++)
+        hashval = *s + 31 * hashval;
+    
+    return hashval % HASHSIZE;
+}
+
+/* lookup : find s in hastable */
+struct nlist *lookup(char *s){
+    struct nlist *np;
+
+    for(np = hastable[hash(s)]; np != NULL; np = np->next)   // standard idiom for walking along a linked list 
+        if(strcmp(s, np->name) == 0)
+            return np; // found!
+    return NULL; // not found
+}
 
 /* talloc */
 struct tnode *talloc(void){
@@ -28,7 +46,22 @@ char *strdup(char *s){
     return p;
 }
 
+// declararations
+struct nlist *lookup(char *);
+char *strdup(char *);
 
+/* install : put(name, defn) in hastab */
+struct nlsit *install(char *name, char *defn){
+    struct nlist *np;
+    unsigned hasval;
+
+    if((np = lookup(name)) == NULL){
+        np = (struct nlist *) malloc(sizeof(*np));
+        if(np==NULL || (np->name = strdup(name)) == NULL)
+            return NULL;
+            
+    }
+}
 
 
 
