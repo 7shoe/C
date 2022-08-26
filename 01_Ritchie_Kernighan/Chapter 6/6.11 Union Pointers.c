ad605ecd0aea2adf9e7d0e27fa11cb64081c67ea
7shoe <carlo.siebenschuh@gmail.com>
2022-08-25 23:40:31 -0500

    Chapter 6, XXI

--- 01_Ritchie_Kernighan/Chapter 6/6.11 Union Pointers.c
+++ 01_Ritchie_Kernighan/Chapter 6/6.11 Union Pointers.c
@@ -21,4 +21,18 @@ int main(){
     printf(" Name       : %s \n", record1.name);
     printf(" Subject    : %s \n", record1.subject);
     printf(" Percentage : %f \n\n", record1.percentage);
+
+    // assigning values to record2 union variable
+    printf("Union record2 values example\n");
+    strcpy(record2.name, "Mani");
+    printf(" Name       : %s \n", record2.name);
+
+    strcpy(record2.subject, "Physics");
+    printf(" Subject    : %s \n", record2.subject);
+
+    record2.percentage = 99.50;
+    printf(" Percentage : %f \n", record2.percentage);
+
+    
+    return 0;
 }
