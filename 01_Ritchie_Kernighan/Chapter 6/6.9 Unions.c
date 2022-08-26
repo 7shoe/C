0cf83162a058b2b2c34bffe40fc23fb67e81fb1f
7shoe <carlo.siebenschuh@gmail.com>
2022-08-25 23:19:39 -0500

    Chapter 6, XVI

--- 01_Ritchie_Kernighan/Chapter 6/6.9 Unions.c
+++ 01_Ritchie_Kernighan/Chapter 6/6.9 Unions.c
@@ -13,7 +13,18 @@ int main( ) {
 
    union Data data;        
 
-   printf( "Memory size occupied by data : %d\n", sizeof(data));
+   printf( "Memory size occupied by data : %d\n", sizeof(data));   
+
+    // set members of Union 
+   data.i = 10;
+   data.f = 220.5;
+   strcpy( data.str, "C Programming");
+
+   printf( "data.i : %d\n", data.i);
+   printf( "data.f : %f\n", data.f);  // final value assigned to the variable has occupied the memory location --> corrupts data.f
+   printf( "data.str : %s\n", data.str);
+
+    // non-corrupted print
 
    return 0;
 }
