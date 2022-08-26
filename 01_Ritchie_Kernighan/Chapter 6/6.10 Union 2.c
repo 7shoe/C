eba81b190dcaffbcf1b4833d36a68b5beefc7de8
7shoe <carlo.siebenschuh@gmail.com>
2022-08-25 23:30:16 -0500

    Chapter 6, XVII

---
+++ 01_Ritchie_Kernighan/Chapter 6/6.10 Union 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+// (!) TL;DR: Structures allocate enough space to store all their members, whereas unions can only hold one member value at a time.
+
+
+
+int main(){
+
+
+
+
+    return 0;
+}
