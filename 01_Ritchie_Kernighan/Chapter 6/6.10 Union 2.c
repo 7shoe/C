b0ac9c123fc309b7f241d32f7cf54be080a4dc5b
7shoe <carlo.siebenschuh@gmail.com>
2022-08-25 23:36:05 -0500

    Chapter 6,  XIX

--- 01_Ritchie_Kernighan/Chapter 6/6.10 Union 2.c
+++ 01_Ritchie_Kernighan/Chapter 6/6.10 Union 2.c
@@ -18,11 +18,26 @@ struct structJob
    int workerNo;        //  4 bytes
 } sJob;                       // so, oversll as the sum, 40 bytes
 
+// simple union
+union Job {
+   float salary;
+   int workerNo;
+} j;
 
 int main(){
 
-    printf("size of union = %d bytes", sizeof(uJob));
-    printf("\nsize of structure = %d bytes", sizeof(sJob));
+    printf("size of union = %d bytes\n", sizeof(uJob));
+    printf("size of structure = %d bytes\n\n", sizeof(sJob));
+
+
+    j.salary = 12.3;
+
+    // when j.workerNo is assigned a value,
+    // j.salary will no longer hold 12.3
+    j.workerNo = 100;
+
+    printf("Salary = %.1f\n", j.salary);
+    printf("Number of workers = %d", j.workerNo);
     
     return 0;
 
