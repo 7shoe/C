99c93d660c7c324be34d8a3157539c8abc9e5e7f
7shoe <carlo.siebenschuh@gmail.com>
2022-08-30 23:05:28 -0500

    Chapter 6, XXXVIII

--- 01_Ritchie_Kernighan/Chapter 6/6.14 Heap's Algo.c
+++ 01_Ritchie_Kernighan/Chapter 6/6.14 Heap's Algo.c
@@ -19,3 +19,20 @@ void print(const int *v){
   }
 }
 
+void heappermute(int v[], int n){
+    int i;
+    if (n == 1) {
+        print(v);
+	}
+    else {
+        for (i = 0; i < n; i++) {
+            heappermute(v, n-1);
+            if (n % 2 == 1) {
+                swap(&v[0], &v[n-1]);
+	    }
+            else {
+                swap(&v[i], &v[n-1]);
+            }
+	}
+    }
+}
