50153f6d4aa5771ee033f1564ec71ab1c675fdcd
7shoe <carlo.siebenschuh@gmail.com>
2022-09-05 23:52:41 -0500

    ttt fix

---
+++ XX_Projects/TicTacToe/02 tictactoe v1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+
+
+
+int main(){
+
+    /* 
+    Tic Tac Toe
+     - 3x3 table, each state has state 'X', 'O', or '-'
+     - each round: player ('X') chooses one of the remaining cells labelled '-'
+     - opponent ('O') chooses of one remaining '-' 
+    */
+
+    return 0;
+}
