d428aa74f6a04b38be138badc8036e759fc1354d
7shoe <carlo.siebenschuh@gmail.com>
2022-09-05 21:41:54 -0500

    ncurses example

--- XX_Projects/TicTacToe/01 ncurses.c
+++ XX_Projects/TicTacToe/01 ncurses.c
@@ -5,5 +5,13 @@
 
 int main(){
 
+    initscr();			            /* Start curses mode 		  */
+	printw("Hello World !!!");	    /* Print Hello World		  */
+	refresh();			            /* Print it on to the real screen */
+	getch();			            /* Wait for user input */
+	endwin();			            /* End curses mode		  */
+
+	return 0;
+
     return 0;
 }
