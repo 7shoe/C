2761a59b31c84fa40075cb8b33c309cd1567a721
7shoe <carlo.siebenschuh@gmail.com>
2022-09-06 23:55:54 -0500

    check table

--- XX_Projects/TicTacToe/02 tictactoe v1.c
+++ XX_Projects/TicTacToe/02 tictactoe v1.c
@@ -2,6 +2,8 @@
 #include <assert.h>
 #include <string.h> 
 
+char tab[10];
+
 /* print the table with the current 'X', 'O' locations */
 void printTable(char * table){
     char out[1024];
@@ -18,11 +20,13 @@ void printTable(char * table){
         }
     }
     printf("%s", out); 
+
+    memset(out, 0, sizeof(out));
 }
 
 /* initialize table with all '-' entrie */
 char * initTable(){
-    static char tab[] = "123456789";
+    strcpy(tab, "123456789");
 
     return tab;
 } 
@@ -31,8 +35,31 @@ char * initTable(){
 void setEntry(char symbol, int index, char * table){
     assert((symbol=='X') || (symbol=='O'));
     assert((index < 9) || (index >= 0));
-    if((table[index]=='X') || (table[index]=='O'))
+    if((table[index]!='X') && (table[index]!='O'))
         table[index] = symbol;
+}
+
+/* check for winner */
+int checkForWinner(char * table){
+    // 0,1,2;  3,4,5;  6,7,8;  0,3,6;  1,4,7;  2,5,8;  0,4,8;  2,4,6
+    // check horizontal
+    for(int i=0; i < 9; i+=3){
+        if((table[i]==table[i+1]) && (table[i+1]==table[i+2]))
+            return 0;
+    }
+    // check vertical
+    for(int i=0; i < 2; ++i){
+        if((table[i]==table[i+3]) && (table[i+3]==table[i+6]))
+            return 0;
+    }
+    // check diagonal
+    if((table[0]==table[4]) && (table[4]==table[8]))
+        return 0;
+    // check anti-diagonal
+    if((table[2]==table[4]) && (table[4]==table[6]))
+        return 0;
+    
+    return -1;
 }
 
 
@@ -50,6 +77,15 @@ int main(){
     char * t = initTable();
 
     printTable(t);
+
+    setEntry('X', 2, t);
+
+    //printTable(t);
+
+    //setEntry('O', 0, t);
+
+    printf("X as int: %d\n", 'X' - '0');
+    printf("O as int: %d\n", 'O' - '0');
 
     return 0;
 }
