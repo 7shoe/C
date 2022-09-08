8b9bae6f0b77129a65276c38e67f5e81a4d1b1e8
7shoe <carlo.siebenschuh@gmail.com>
2022-09-08 00:10:46 -0500

    Chapter 7, XV

--- 01_Ritchie_Kernighan/Chapter 7/7.4 String functions.c
+++ 01_Ritchie_Kernighan/Chapter 7/7.4 String functions.c
@@ -12,11 +12,29 @@ int main(){
 
     sprintf(name, ", Age: %d, Salary: %d", alfred_age, salary);  // (!) name will be overwritten by the latter string
 
-    printf("Info: \n%s", name);
+    printf("Info: \n%s\n", name);
 
-    // memc
+    // strcpy
+    char original[100] = "Bruce Wayne";
+    char copy[100];
+    char limitedCopy[12];
 
+    strcpy(copy, original);
+    strcpy(limitedCopy, original);
+    printf("The Original  : %s\n", original);
+    printf("The Copy      : %s\n", copy);
+    printf("Limited Copy  : %s\n\n", limitedCopy);
     
+    // strcat
+    char part1[50], part2[50];
+
+    strcpy(part1, "This is not the end... ");
+    strcpy(part2, "but a new beginning.");
+
+    strcat(part1, part2); // strcat in action
+
+    printf("stract: %s\n\n", part1);
+    printf("stract: %s\n\n", part1);
 
     return 0;
 }
