e82d03036014b4ac96b083a577d5ca42c63e3e7e
7shoe <carlo.siebenschuh@gmail.com>
2022-09-07 23:58:14 -0500

    Chapter 7, XIV

--- 01_Ritchie_Kernighan/Chapter 7/7.4 String functions.c
+++ 01_Ritchie_Kernighan/Chapter 7/7.4 String functions.c
@@ -5,14 +5,16 @@
 int main(){
 
     // sprintf
-    char name[100];
+    char name[100] = "Alfred PennyWorth";
     char position[] = ", Buttler";
     int alfred_age = 66;
     int salary = 5000;
 
-    sprintf(name, ", Age: %d, Salary: %d", alfred_age, salary);
+    sprintf(name, ", Age: %d, Salary: %d", alfred_age, salary);  // (!) name will be overwritten by the latter string
 
-    printf("Info: \n", name);
+    printf("Info: \n%s", name);
+
+    // memc
 
     
 
