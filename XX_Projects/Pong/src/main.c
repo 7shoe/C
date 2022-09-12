78824b0212fabe6c87c1b1ce0c211354501009b5
7shoe <carlo.siebenschuh@gmail.com>
2022-09-12 15:12:11 -0500

    minor fixes

--- XX_Projects/Pong/src/main.c
+++ XX_Projects/Pong/src/main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
-#include <SDL_image.h>
-#include <SDL2/SDL_image.h>
+//#include <SDL_image.h>
 
 #define WIDTH 640
 #define HEIGHT 480
 
 SDL_Texture *LoadTexture(char * filePath, SDL_Renderer *renderTarget){
     SDL_Texture *texture = NULL;
-    SDL_Surface *surface = IMG_Load(filePath);
+    //SDL_Surface *surface = IMG_Load(filePath);
 
     if(surface == NULL){
         printf("SDL_Init failed with error: %s", SDL_GetError());
@@ -37,15 +36,15 @@ int main(){
 
     SDL_Init(SDL_INIT_VIDEO);
 
-    int imgFlags = IMG_INIT_PNG | IMG_INIT_JPG;
+    int imgFlags = 0; // IMG_INIT_PNG | IMG_INIT_JPG;
 
-    if (!(IMG_Init(imgFlags) & imgFlags)){
-        printf("SDL_Init failed with error: %s", SDL_GetError());
-    }
+    //if (!(IMG_Init(imgFlags) & imgFlags)){
+    //    printf("SDL_Init failed with error: %s", SDL_GetError());
+    //}
 
     window = SDL_CreateWindow("SDL Window created!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     renderTarget = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    currentImage = IMG_LoadTexture("./data/sample_pong.bmp", renderTarget);
+    //currentImage = IMG_LoadTexture("./data/sample_pong.bmp", renderTarget);
 
     bool isRunning = true;
     SDL_Event event;
@@ -59,12 +58,12 @@ int main(){
     }
 
     SDL_RenderClear(renderTarget);
-    SDL_RenderCopy(renderTarget, currentImage, NULL, NULL);
+    //SDL_RenderCopy(renderTarget, currentImage, NULL, NULL);
     SDL_RenderPresent(renderTarget);
 
     // clean-up
     SDL_DestroyWindow(window);
-    SDL_DestroyTexture(currentImage);
+    //SDL_DestroyTexture(currentImage);
     SDL_DestroyRenderer(renderTarget);
 
     SDL_Quit();
