cefa5785bd80c31db57a1649ae895577640a4e51
7shoe <carlo.siebenschuh@gmail.com>
2022-09-25 23:58:36 -0500

    bug fixed

--- 01_Ritchie_Kernighan/Chapter 7/7.12 Regression.c
+++ 01_Ritchie_Kernighan/Chapter 7/7.12 Regression.c
@@ -6,7 +6,7 @@ int main(){
     float x[5]={60,61,62,63,65},y[5]={3.1,3.6,3.8,4.0,4.1};
     double sum_y=0,sum_xy=0,sum_x=0,sum_xx=0,sum_x2=0,slope=0,intercept=0,reg;
     int i,n=5;
-    
+
     for(i=0;i<n;i++){
         xx[i]=x[i]*x[i];
         yy[i]=y[i]*y[i];
@@ -22,8 +22,10 @@ int main(){
     sum_x2=sum_x*sum_x;
     dr=(n*sum_xx)-sum_x2;
     slope=nr/dr;
+
     double fac = pow(10, 2);
-    slope=round(slope*fac)/fac;       
+    slope=round(slope*fac)/fac;    
+       
     intercept=(sum_y- slope*sum_x)/n;
     reg= intercept + (slope*64);
     printf("Slope: %.2f\nIntercept: %.3f\nRegression: %.3f",slope,intercept,reg);
