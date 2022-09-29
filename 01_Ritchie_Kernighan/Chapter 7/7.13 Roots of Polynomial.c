91cfa01679e61c6aeb65665f323b7e643c70c887
7shoe <carlo.siebenschuh@gmail.com>
2022-09-29 00:19:35 -0500

    fixed read-in bugs

--- 01_Ritchie_Kernighan/Chapter 7/7.13 Roots of Polynomial.c
+++ 01_Ritchie_Kernighan/Chapter 7/7.13 Roots of Polynomial.c
@@ -11,15 +11,15 @@ int main(){
 
     printf("> ");
     a = getchar();
-    printf("Got a: %d", a);
+    printf("Got a: %d", scanf("%d", &a));
 
     printf("\n> ");
     b = getchar();
-    printf("Got b: %d", b);
+    printf("Got b: %d", scanf("%d", &b));
 
     printf("\n> ");
     c = getchar();
-    printf("Got c: %d", c);
+    printf("Got c: %d", scanf("%d", &c));
     printf("\n\n Quadratic Polynomial is: %dx^{2} + %dx + %d\n", a, b, c);
 
     // compute discriminant
