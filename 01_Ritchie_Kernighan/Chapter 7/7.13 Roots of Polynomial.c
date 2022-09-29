85babe53a4e66344662713ccdf6067c56da03cf2
7shoe <carlo.siebenschuh@gmail.com>
2022-09-28 23:53:11 -0500

    real roots added

--- 01_Ritchie_Kernighan/Chapter 7/7.13 Roots of Polynomial.c
+++ 01_Ritchie_Kernighan/Chapter 7/7.13 Roots of Polynomial.c
@@ -7,6 +7,7 @@ int main(){
 
     // read parameters
     int a, b, c;
+    float d, x1, x2;
 
     printf("> ");
     a = getchar();
@@ -22,8 +23,12 @@ int main(){
     printf("\n\n Quadratic Polynomial is: %dx^{2} + %dx + %d\n", a, b, c);
 
     // compute discriminant
-    
+    d = b*b - 4*a*c;
     // compute roots
+    if(d > 0){
+        x1 = ((-1.0) * b + sqrt(d)) / (2 * a);
+        x2 = ((-1.0) * b - sqrt(d)) / (2 * a);
+    }
 
 
 
