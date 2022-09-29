1d7ec568582c5ed80f07cef819dabae512a3de7a
7shoe <carlo.siebenschuh@gmail.com>
2022-09-28 23:47:52 -0500

    read paras

---
+++ 01_Ritchie_Kernighan/Chapter 7/7.13 Roots of Polynomial.c
@@ -0,0 +1,27 @@
+#include <math.h>
+#include <stdio.h>
+
+
+
+int main(){
+
+    // read parameters
+    int a, b, c;
+
+    printf("> ");
+    a = getchar();
+    printf("Got a: %d", scanf("%d", &a));
+
+    printf("\n> ");
+    b = getchar();
+    printf("Got b: %d", scanf("%d", &b));
+
+    printf("\n> ");
+    c = getchar();
+    printf("Got c: %d", scanf("%d", &c));
+
+
+
+
+    return 0;
+}
